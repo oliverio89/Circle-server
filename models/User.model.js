089f7062f4bc84169b986c2d9d5db8e0cfb0aab6
YanxiaWu <yanxiawusilva@gmail.com>
2022-12-06 15:17:59 +0100

    Modal

--- models/User.model.js
+++ models/User.model.js
@@ -5,26 +5,27 @@ const userSchema = new Schema(
   {
     name: {
       type: String,
-      // required: true,
-      // unique: true
+      required: true,
+      unique: true
     },
     email: {
       type: String,
-      // required: true,
-      // unique: true,
-      // lowercase: true
+      required: true,
+      unique: true,
+      lowercase: true
     },
     password: {
       type: String,
-      // required: true
+      required: true
     },
     username: {
       type: String,
-      // required: true
+      required: true
     },
     imageUrl: {
       type: String,
-      // required: true
+      default: 'hola mundo'
+
     },
     bio: {
       type: String
