3daf6ba5f1f5e4f640ec0a5a54d358c3b59692df
YanxiaWu <yanxiawusilva@gmail.com>
2022-12-09 18:43:27 +0100

    routes

--- routes/posts.routes.js
+++ routes/posts.routes.js
@@ -73,6 +73,8 @@ router.put("/likePost/:comenData", isAuthenticated, (req, res, next) => {
 
 
 
+
+
 router.delete("/deletePost/:post_id", isAuthenticated, (req, res, next) => {
 
     const { post_id } = req.params
