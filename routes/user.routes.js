598cca7b91e1eecc25a5cd746f0c83238cf0c0db
victor moreno <oliverio89@gmail.com>
2022-12-09 12:42:54 +0100

    first friend

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,4 @@
 const router = require("express").Router()
-
 const { isAuthenticated } = require("../middleware/jwt.middleware")
 const User = require('./../models/User.model')
 
@@ -22,8 +21,25 @@ router.delete("/deleteUser/:user_id", isAuthenticated, (req, res, next) => {
         .findByIdAndDelete(user_id)
         .then(response => res.json(response))
         .catch(err => next(err))
+})
 
+// add new friend
+
+router.post('/addFriend', (req, res, next) => {
+    const { user_id } = req.body
+    const { _id: owner } = req.payload
+
+
+    User
+        .findByIdAndUpdate(user_id, { "$push": { "friends": owner } })
+        .then()
+        .catch((err) => next(err))
 
 })
 
-module.exports = router 
+
+module.exports = router
+
+
+
+
