80c6260519caef9e8a0d484a1564f23c517f7e1a
yoaomoreira <jpmoreiraes@gmail.com>
2022-12-12 17:22:00 +0100

    post routes

--- routes/posts.routes.js
+++ routes/posts.routes.js
@@ -54,64 +54,65 @@ router.post("/savePost", isAuthenticated, (req, res, next) => {
                 .catch(err => res.status(500).json(err))
 
         })
+})
 
-    router.put("/editPost/:post_id", isAuthenticated, (req, res, next) => {
-        const { post_id } = req.params
-        const { title, description, imageUrl } = req.body
-
-
-        Post
-            .findByIdAndUpdate(post_id, { title, description, imageUrl })
-            .then(response => res.json(response))
-            .catch(err => next(err))
-    })
-
-    router.put("/likePost/:comenData", isAuthenticated, (req, res, next) => {
-        const currentUser = req.payload._id
-        const comenData = req.params
-
-        Post
-            .findByIdAndUpdate(comenData.comenData, { $addToSet: { 'likes': currentUser } }, { new: true })
-            .then(response => res.json(response))
-            .catch(err => next(err))
-    })
-
-    // quitar el like
-
-    router.put("/dislikePost/:disLikeData", isAuthenticated, (req, res, next) => {
-        const currentUser = req.payload._id
-        const disLikeData = req.params
-
-        Post
-            .findByIdAndUpdate(disLikeData.disLikeData, { $pull: { 'likes': currentUser } }, { new: true })
-            .then(response => res.json(response))
-            .catch(err => next(err))
-    })
-
-    //reportar un post
-
-    router.put("/reportPost/:report", isAuthenticated, (req, res, next) => {
-        const currentUser = req.payload._id
-        const report = req.params
-
-        Post
-            .findByIdAndUpdate(report.report, { $addToSet: { 'reportes': currentUser } }, { new: true })
-            .then(response => res.json(response))
-            .catch(err => next(err))
-    })
+router.put("/editPost/:post_id", isAuthenticated, (req, res, next) => {
+    const { post_id } = req.params
+    const { title, description, imageUrl } = req.body
 
 
-    router.delete("/deletePost/:post_id", isAuthenticated, (req, res, next) => {
+    Post
+        .findByIdAndUpdate(post_id, { title, description, imageUrl })
+        .then(response => res.json(response))
+        .catch(err => next(err))
+})
 
-        const { post_id } = req.params
+router.put("/likePost/:comenData", isAuthenticated, (req, res, next) => {
+    const currentUser = req.payload._id
+    const comenData = req.params
 
-        Post
-            .findByIdAndDelete(post_id)
-            .then(response => res.json(response))
-            .catch(err => next(err))
+    Post
+        .findByIdAndUpdate(comenData.comenData, { $addToSet: { 'likes': currentUser } }, { new: true })
+        .then(response => res.json(response))
+        .catch(err => next(err))
+})
+
+// quitar el like
+
+router.put("/dislikePost/:disLikeData", isAuthenticated, (req, res, next) => {
+    const currentUser = req.payload._id
+    const disLikeData = req.params
+
+    Post
+        .findByIdAndUpdate(disLikeData.disLikeData, { $pull: { 'likes': currentUser } }, { new: true })
+        .then(response => res.json(response))
+        .catch(err => next(err))
+})
+
+//reportar un post
+
+router.put("/reportPost/:report", isAuthenticated, (req, res, next) => {
+    const currentUser = req.payload._id
+    const report = req.params
+
+    Post
+        .findByIdAndUpdate(report.report, { $addToSet: { 'reportes': currentUser } }, { new: true })
+        .then(response => res.json(response))
+        .catch(err => next(err))
+})
 
 
-    })
+router.delete("/deletePost/:post_id", isAuthenticated, (req, res, next) => {
+
+    const { post_id } = req.params
+
+    Post
+        .findByIdAndDelete(post_id)
+        .then(response => res.json(response))
+        .catch(err => next(err))
 
 
-    module.exports = router 
+})
+
+
+module.exports = router 
